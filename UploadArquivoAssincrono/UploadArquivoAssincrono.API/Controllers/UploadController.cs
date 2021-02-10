71d664a5dcd97f3bf42a0e0bf0f9aa304fd8d7b1
Bruno Fernando <bruno.abreu@iteris.com.br>
2021-02-09 21:33:13 -0300

    Adicionado projeto angular e web api .net core para upload do arquivo excel

---
+++ UploadArquivoAssincrono/UploadArquivoAssincrono.API/Controllers/UploadController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UploadArquivoAssincrono.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UploadController : ControllerBase
+    {
+        private readonly ILogger<UploadController> _logger;
+
+        public UploadController(ILogger<UploadController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [Route("excel")]
+        public async Task<IActionResult> Excel(IFormFile file, CancellationToken cancellationToken)
+        {
+            bool sucesso;
+            try
+            {
+                sucesso = await this.processarArquivo(file);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+
+            return Ok(sucesso);
+        }
+
+        private async Task<bool> processarArquivo(IFormFile arquivo)
+        {
+            bool sucesso;
+            try
+            {
+                var extension = "." + arquivo.FileName.Split('.')[arquivo.FileName.Split('.').Length - 1];
+                string fileName = $"{DateTime.Now.Day}-{DateTime.Now.Month}-{DateTime.Now.Year}-{DateTime.Now.Hour}-{DateTime.Now.Minute}{extension}";
+
+                var pathBuilt = Path.Combine(Directory.GetCurrentDirectory(), "Upload\\files");
+
+                if (!Directory.Exists(pathBuilt))
+                {
+                    Directory.CreateDirectory(pathBuilt);
+                }
+
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "Upload\\files",
+                   fileName);
+
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    await arquivo.CopyToAsync(stream);
+                }
+
+                sucesso = true;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+            return sucesso;
+        }
+    }
+}
